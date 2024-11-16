cbd913dfc27f17f9356a1647ec10a6ba28ae9e9a
utku <utkbktss5@gmail.com>
2024-11-16 22:55:49 +0300

    fixed(axios): deploy fixed

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,10 +1,7 @@
 import _axios from "axios";
 
 const axiosInstance = _axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5000/api/v1"
-      : "/api/v1",
+  baseURL: import.meta.env.VITE_REACT_APP_URL || "/api/v1",
   withCredentials: true,
 });
 
